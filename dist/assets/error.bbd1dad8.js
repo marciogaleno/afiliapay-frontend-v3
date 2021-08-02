18e6f213ba449713e3d801f9d72d200870aad30d
Marcio Galeno <marciovennan@gmail.com>
2021-08-02 20:04:00 -0300

    teste

---
+++ dist/assets/error.bbd1dad8.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.bd34858d.js";import{i as o,o as r,H as s,t,s as n}from"./vendor.2e4d4237.js";const a={};a.render=function(a,d){const i=o("RouterView"),u=e;return r(),s(u,null,{default:t((()=>[n(i)])),_:1})};export default a;
--- dist/assets/onboarding-welcome.f3ac6aaa.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./OnboardingStandard.f7e66052.js";import{d as e,G as o,o as r,e as t,s}from"./vendor.2e4d4237.js";import{p as n}from"./navbarLayoutState.a1178f68.js";import"./index.799c8018.js";const d={class:"page-content-inner"};var i=e({setup:e=>(n.value="Welcome",o({title:"Welcome - Navbar - Vuero"}),(e,o)=>{const n=a;return r(),t("div",d,[s(n)])})});export default i;
